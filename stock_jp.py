bfeec99885d1efd9a6249d8c10a571997e853f09
20007 <74938961+ai20007@users.noreply.github.com>
2022-12-27 15:44:56 +0900

    Add files via upload

--- stock_jp.py
+++ stock_jp.py
@@ -1,22 +1,26 @@
 import os
+import time
 import pandas_datareader.data as web
 import pandas as pd
+
 
 # 銘柄DF化
 def list_jp():
     stock_list_df = pd.read_excel("./data_j.xls")
     return stock_list_df
 
+
 # データ保存
 def data_jp(ticker_symbol):
-    start = '2012-01-01' # 開始日
-    end = '2021-12-31' # 終了日
-    ticker_symbol_dr = str(ticker_symbol) + ".JP" # 銘柄コード
-    df = web.DataReader(ticker_symbol_dr, data_source='stooq', start=start, end=end) # データ取得
-    df.to_csv(os.path.dirname(__file__) + '\s' + str(ticker_symbol) + '_jp.csv') # csv保存
+    start = '2012-01-01'  # 開始日
+    end = '2021-12-31'  # 終了日
+    ticker_symbol_dr = str(ticker_symbol) + ".JP"  # 銘柄コード
+    df = web.DataReader(ticker_symbol_dr, data_source='stooq', start=start, end=end)  # データ取得
+    df.to_csv(os.path.dirname(__file__) + '\s' + str(ticker_symbol) + '_jp.csv')  # csv保存
+
 
 # スクレイピング
-def main(time):
+def main(wait_time):
     stock_list = list_jp()
     length = len(stock_list)
     # DFをndarrayに変換して高速化
@@ -27,10 +31,9 @@ def main(time):
         if stock_list_group[i] == ("プライム（内国株式）" or "スタンダード（内国株式）" or "グロース（内国株式）"):
             code_num = stock_list_code[i]
             data_jp(code_num)
-            time.sleep(time)
+            time.sleep(wait_time)
 
 
 # プログラムを自動で走らせる間隔(秒)を設定
-span = 60 * 5
 if __name__ == "__main__":
-    main(span)
+    main(300)
